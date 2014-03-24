d99e2dbcde3481aebc864fc43c63dba11e23dd25
zhaoguo20002 <zhaoguo2004@126.com>
2014-03-24 14:07:46 +0800

    加快冲刺的速度

--- client/project/js/mvc/controllers/controllerWorld.js
+++ client/project/js/mvc/controllers/controllerWorld.js
@@ -120,7 +120,7 @@ define([
 			var _model = this.view.model, _getSuperStar = _model.world.getSuperStar();
 			if (_getSuperStar && _model.lockedRole) {
 				_getSuperStar.setStep(1).setMoveDs(_model.sprintDs).setStopDs(_model.stopDs);
-				_model.world.setCameraSpeed(_model.tw, _model.th).jump(_getSuperStar.id, _model.lockedRole.x, _model.lockedRole.y, 1, 1, 15);
+				_model.world.setCameraSpeed(_model.tw, _model.th).jump(_getSuperStar.id, _model.lockedRole.x, _model.lockedRole.y, 0, 1, 5);
 			}
 			_model = _getSuperStar = null;
 			return this;
